a4b385ed66cd4f9fecf6438470f1bdb35f2b0abd
Kevin <kevinrezende55@outlook.com>
2026-06-13 22:53:54 -0300

    ignora commit, atualizando codigo com a main

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,7 @@ function App() {
 
   return (
     <main className="app">
-      {/* 
-        No Vite, o React Router funciona do lado do cliente.
-        O BrowserRouter fica no main.jsx envolvendo o App inteiro.
-        Assim, toda a aplicacao consegue usar rotas, links e navegacao.
-      */}
       <nav className="menu">
-        {/* 
-          Link troca a rota sem recarregar a pagina, diferente de uma tag <a>.
-          Use Link quando a navegacao aparece direto na tela, como menu ou botoes de pagina.
-        */}
         <Link to="/">Home</Link>
         {estaAutenticado && (
           <>
@@ -41,20 +32,8 @@ function App() {
             <Link to="/cadastro">Cadastro</Link>
           </>
         )}
-        {/* <Link to="/planetas">Planetas</Link> */}
-
-
-        {/*
-          useNavigate permite navegar por codigo.
-          Use quando a navegacao depende de uma acao, como login concluido,
-          cadastro finalizado ou clique em um botao com alguma regra antes.
-        */}
-        {/* <button type="button" onClick={() => navigate("/aliens")}>
-          Ir para Aliens
-        </button> */}
       </nav>
 
-      {/* AppRouter renderiza a pagina correspondente a rota atual da URL. */}
       <AppRouter />
     </main>
   );
